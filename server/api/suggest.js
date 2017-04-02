516cbfb73413f6ee2d5ad1d1d2bf9cbb829ab3cf
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-04-02 17:40:50 +0530

    Suggest API module complete Small changes in dummy data variables

--- server/api/suggest.js
+++ server/api/suggest.js
@@ -44,10 +44,92 @@ router.post('/nearestStop', function(req, res) {
 router.post('/bus', function(req, res) {
   var query = req.body;
   if (
-    sanity_check.isRequired(query.dest)
+    sanity_check.isRequired(query.dest) &&
+    sanity_check.isRequired(query.coord) &&
+    sanity_check.isRequired(query.coord.lat) &&
+    sanity_check.isRequired(query.coord.lng)
   ) {
     var db = req.db;
-    
+    if (query.src) {
+
+      var collection = db.get("BusStop");
+      collection.findOne({"stopName": query.dest},{},function(err, record) {
+        if (err) {
+          console.error(err);
+          res.status(500);
+          res.send();
+          return;
+        }
+        var busesAtDest = record.busList;
+        collection.findOne({"stopName": query.src},{}, function(err, element) {
+          if (err) {
+            console.error(err);
+            res.status(500);
+            res.send();
+            return;
+          }
+          var busesAtSrc = element.busList;
+          var suggestedBuses = busesAtDest.filter((n) => {
+                            return busesAtSrc.indexOf(n) !== -1;
+                          });
+          var busCollection = db.get("Bus");
+          busCollection.find(
+            {busNo : { $in : suggestedBuses}},
+            {},
+            function(err, documents) {
+              if (err) {
+                console.error(err);
+                res.status(500);
+                res.send();
+                return;
+              }
+              documents = documents.map((document) => {
+                var time;
+                for (var i=0; i<document.Timings.length; i++) {
+                  if (document.Timings[i].busStop === query.src) {
+                    time = document.Timings[i].time;
+                    break;
+                  }
+                }
+                return ({
+                  "bus_no" : document["busNo"],
+                  "src" : query.src,
+                  "time" : time
+                });
+              });
+              documents.sort((A,B) => {
+                return A.time - B.time;
+              });
+              var date = new Date();
+              var threshold = date.getHours()*60 + date.getMinutes();
+
+              console.log(documents);
+
+              documents = documents.filter((document) => {
+                return (document.time >= threshold);
+              });
+              // Send Top Five Results
+              res.send(documents.slice(0,5));
+            });
+        });
+      });
+
+    } else {
+      var processing = util.sourceAndBusSuggest(db, query.coord, query.dest);
+      processing.then(
+        (result) => {
+          res.send(result);
+        }
+      )
+      .catch(
+        (reason) => {
+          console.error(reason);
+          res.status(500);
+          res.send();
+        }
+      )
+    }
+
   } else {
     res.status(400);
     res.send();
