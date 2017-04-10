d8ace2bb8992619b22c5aabb1e9adbd67c8e7868
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-04-10 17:07:04 +0530

    Working interface elements

--- phonegap-app/whenBus/www/js/app/map.js
+++ phonegap-app/whenBus/www/js/app/map.js
@@ -2,7 +2,6 @@ if ("geolocation" in navigator) {
   /* geolocation is available */
 
   function addMarker(coord, title, map, infoWindow) {
-    console.log(title)
     marker = new google.maps.Marker({
         position: coord,
         title : title,
@@ -38,28 +37,83 @@ if ("geolocation" in navigator) {
 
     var infoWindow = new google.maps.InfoWindow();
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: center
     });
 
-    // addMarker(center, 'Current Location',map, infoWindow);
+    console.log('Called');
 
-    var busStop =  {lat: 12.98624, lng: 80.248201};
+    var id = localStorage.getItem("whenBusMapId");
 
-    // addMarker(busStop, 'Nearest Bus Stop', map, infoWindow);
+    if (id === '1') {
 
-    var directionsService = new google.maps.DirectionsService,
-        directionsDisplay = new google.maps.DirectionsRenderer({
-                              map: map
-                            });
+      // var busStop =  {lat: 12.98624, lng: 80.248201};
 
-    var PointA = new google.maps.LatLng(center.lat, center.lng),
-        PointB = new google.maps.LatLng(busStop.lat, busStop.lng);
+      var output = {
+        "distance": 102,                        
+        "stopName": 'GC',                       
+        "coord" : {                               
+          "lat" : 12.98624,
+          "lng" : 80.248201
+        }
+      }
 
-    calculateAndDisplayRoute(directionsService, directionsDisplay, PointA, PointB);
+      var busStop = output.coord;
+
+      // addMarker(center, 'Current Location',map, infoWindow);
+      // addMarker(busStop, 'Nearest Bus Stop', map, infoWindow);
+
+      var directionsService = new google.maps.DirectionsService,
+          directionsDisplay = new google.maps.DirectionsRenderer({
+                                map: map
+                              });
+
+      var PointA = new google.maps.LatLng(center.lat, center.lng),
+          PointB = new google.maps.LatLng(busStop.lat, busStop.lng);
+
+      calculateAndDisplayRoute(directionsService, directionsDisplay, PointA, PointB);
+
+      $("#map_data").html('\
+        <ul class="list-group">\
+          <li class="list-group-item list-group-item-info">Nearest Stop Information</li>\
+          <li class="list-group-item list-group-item-basic"> \
+          <pre>Name     : '+output.stopName+'</pre>\
+          <pre>Distance : '+(Math.round( output.distance /100 ) / 10)+'Km</pre></li>\
+        </ul>\
+      ');
+
+    } if (id == '2') {
+
+      var output = {
+        "id" : 1,                          
+        "stop" : "GC",                     
+        "busLoc" : {                               
+          "lat" : 12.98624,
+          "lng" : 80.248201
+        },
+        "time" : 330,                           
+        "stopList" : ["A","B"]                
+      }
+
+      addMarker(center, "Your Location", map, infoWindow);
+      addMarker(output.busLoc, "Bus's Location", map, infoWindow);
+
+
+      $("#map_data").html('\
+        <ul class="list-group">\
+          <li class="list-group-item list-group-item-info">Next Bus Information</li>\
+          <li class="list-group-item list-group-item-basic"> \
+          <pre>Bus Number : '+localStorage.getItem("busNo")+'</pre>\
+          <pre>Stop       : '+output.stop+'</pre>\
+          <pre>ETA        : '+Math.round(output.time/60)+':'+(output.time%60)+'</pre></li>\
+        </ul>\
+        ');                                         
+
+    }
 
     google.maps.event.addDomListener(window, 'load', initMap );
+    setTimeout(initMap, 39000);
   }
 
 
